0bf33b7dca6accb904423c7d6a8bd6013acba89d
Oussama Bastamy <obastamy@enseirb-matmeca.fr>
2021-03-20 20:02:00 +0100

    preamble prob done

--- tests/python/frame_synchro.py
+++ tests/python/frame_synchro.py
@@ -1,24 +1,21 @@
 import sys
 sys.path.insert(0, '../../build/lib')
 sys.path.insert(0, '../../py_aff3ct/build/lib') 
-
+import py_aff3ct
 from py_aff3ct.module.py_module import Py_Module
 import numpy as np
-import py_aff3ct
 from scipy import signal
 import matplotlib.pyplot as plt
 import math
 import numpy.random as rd
+import preamble
 
-###
 class Frame_Synchronizer(Py_Module):
-    size_header=64 #header size in symbols (cst to def) 
-    size= 1000  #data size in symbols (cst to def)
-    bps=2
-    buff=np.zeros(size_header-1) #keep incomplete corr
-
+    
+ # Conjugated and "turned-around" version of the actual header ) to define when framing
+    
     def synchronize(self,s_recv,s_out):
-        HEADER =np.ones(size_header) # size_header complex values conjugate and "turned-around" version of the actual header ) to define when framing
+        
         
         re=np.zeros(s_recv.size//2)
         im=np.zeros(s_recv.size//2)
@@ -28,64 +25,90 @@ class Frame_Synchronizer(Py_Module):
             im[i]=s_recv[0][2*i+1]
         re=s_recv[0][0::2]
         im=s_recv[0][1::2]
+        
+        sig=re+1j*im      
+        print(s_recv) 
+        corr=np.correlate(sig,self.HEADER,"full")
 
-        ###
-        sig=re+1j*im       
-        corr=np.correlate(sig,HEADER,"full")
-
-        tmp=np.zeros(np.size(corr))
-        tmp[np.size(sig):]=Frame_Synchronizer.buff
+        tmp=np.zeros(np.size(corr),dtype=complex)
+        tmp[np.size(sig):]=self.buff
         
         corr_fin=abs(np.add(corr,tmp))
 
         #update buff
-        buff=corr[sig.size-1:] 
+        self.buff=corr[sig.size-1:] 
 
-        #print("corr:", np.add(corr,tmp))
+        #correlation
         delay=np.where(corr_fin-np.amax(corr_fin)==0)
         
-        #print("delay is:",delay[0][0])
+        d=np.shape(sig)
         
-        sig[0:delay[0][0]]=0
-        s_out[0][0::2]=np.real(sig)
-        s_out[0][1::2]=np.imag(sig)
+        self.frame[:d[0]-delay[0][0]]=sig[delay[0][0]:]
+        #output
+        s_out[0][0::2]=np.real(self.frame)
+        s_out[0][1::2]=np.imag(self.frame)
+
+       #
+        self.frame[self.frame.size-delay[0][0]:]=sig[:delay[0][0]]
+        
         return 0     
+    
+    def reset_mem(self):  #reset mem
+        self.buff=np.zeros(size_header-1) 
+        self.frame=np.zeros(size+size_header) 
+        return 0
 
-    def __init__(self,window,len):
-
+    
+    def __init__(self,window,len,header):
+        self.len=len
+        self.HEADER=header[0]
+        self.size_header=header.size
+        self.buff=np.zeros(self.size_header-1,dtype=complex)  #keep incomplete corr
+        self.frame=np.zeros(self.len//2,dtype=complex) #buffer to temporarly store the frame 
+        
+        
         Py_Module.__init__(self)
         self.name = "Frame_Synchronizer"
         tr_synch = self.create_task("tr_synchronize")
-        s_in_sig= self.create_socket_in (tr_synch, "IN", window, np.float32)
-        s_out= self.create_socket_out(tr_synch,"OUT", window, np.float32)
+        s_in_sig= self.create_socket_in (tr_synch, "IN", window, np.float32)  #preferably input window should be smaller thacn frame size bigger than data size
+        s_out= self.create_socket_out(tr_synch,"OUT", len, np.float32)   #return frame size
       
         self.create_codelet(tr_synch, lambda slf, lsk, fid: slf.synchronize(lsk[s_in_sig],lsk[s_out]))
 
 
-window=2000 
+window=2048
 size_header=64 #header size in symbols (cst to def) 
 size= 1000  #data size in symbols (cst to def )
 bps=2
 
-src = py_aff3ct.module.source.Source_random(window)
-cstl = py_aff3ct.tools.constellation.Constellation_QAM(bps)
-mdm  = py_aff3ct.module.modem.Modem_generic(window, cstl)
-fr_syn=Frame_Synchronizer(window,size+size_header)
 
-#bits=np.concatenate(np.ones(size_header*bps),np.zeros(window-size_header*bps))
-#print(bits.size)
-mdm("modulate").info()
+
+src = py_aff3ct.module.source.Source_random(size)
+cstl = py_aff3ct.tools.constellation.Constellation_QAM(bps)
+mdm  = py_aff3ct.module.modem.Modem_generic(size, cstl)
+pre=preamble.preamble(size_header,size)
+head=pre.header 
+
+fr_syn=Frame_Synchronizer(size+2*size_header,size+2*size_header,head)
+
+
+#src("generate").info()
+pre("insert_preamble").info()
+#mdm("modulate").info()
 fr_syn("tr_synchronize").info()
 
+
 mdm["modulate::X_N1"].bind(src["generate::U_K"])
-#mdm["modulate::X_N1"].bind(bits)
-fr_syn["tr_synchronize::IN"].bind(mdm["modulate::X_N2"])
+pre["insert_preamble::s_in"].bind(mdm["modulate::X_N2"])
+fr_syn['tr_synchronize::IN'].bind(pre["insert_preamble::s_out"])
 
 
 #src('generate').debug = True
 #mdm('modulate').debug = True
-fr_syn('tr_synchronize').debug = True
+#fr_syn('tr_synchronize').debug = True
+#pre('insert_preamble').debug=True
 
 src("generate").exec()
 mdm("modulate").exec()
 fr_syn("tr_synchronize").exec()
+pre("insert_preamble").exec()
--- tests/python/preamble.py
+++ tests/python/preamble.py
@@ -11,7 +11,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sys  
-sys.path.insert(0, './py_aff3ct/build/lib')
+sys.path.insert(0, '../../py_aff3ct/build/lib')
 import py_aff3ct
 from py_aff3ct.module.py_module import Py_Module
 
@@ -19,7 +19,9 @@ from py_aff3ct.module.py_module import Py_Module
 #the preamble is composed of BPSK modulated syms in this sequence 1 j -1 -j .... 
 
 class preamble(Py_Module):
-
+    
+    
+    
 
     def create_preamble(self):        
         pream_cp=np.zeros((self.len_pream,1),dtype=np.complex64)
@@ -32,9 +34,9 @@ class preamble(Py_Module):
         return out
 
     def insert_preamble(self, in_, out_):
-        pream=self.create_preamble()
-        out_=np.concatenate((np.transpose(pream),np.transpose(in_)), axis=0)
-        out_=np.transpose(out_)
+
+        out_[:]=np.transpose(np.concatenate((np.transpose(self.header),np.transpose(in_)), axis=0))
+        
         return 0
    
 
@@ -47,78 +49,16 @@ class preamble(Py_Module):
         self.name = 'preamble'
         self.len_pream = len_pream
         self.len_frame=len_frame
-        t_create_pream=self.create_task('create_preamble')
-        self.create_codelet(t_create_pream, lambda slf, lsk, fid:self.create_preamble())
-
+        self.header=self.create_preamble()
 
         t_ins_pream = self.create_task('insert_preamble')
-        sin = self.create_socket_in(t_ins_pream, "s_in",self.len_frame,np.float32)
-        sout = self.create_socket_out(t_ins_pream, "s_out", self.len_frame+self.len_pream, np.float32)
+        sin = self.create_socket_in(t_ins_pream, "s_in",len_frame,np.float32)
+        sout = self.create_socket_out(t_ins_pream, "s_out", len_frame+2*len_pream, np.float32)
         self.create_codelet(t_ins_pream,lambda slf, lsk, fid: self.insert_preamble(lsk[sin], lsk[sout]))
 
         t_rem_pream=self.create_task('remove_preamble')
-        sin=self.create_socket_in(t_rem_pream, "s_in",self.len_frame+self.len_pream,np.float32)
-        sout=self.create_socket_out(t_rem_pream, "s_out",self.len_frame,np.float32)
-        self.create_codelet(t_rem_pream, lambda slf, lsk, fid: self.remove_preamble(lsk[sin], lsk[sout]))
+        s_in=self.create_socket_in(t_rem_pream, "s_in",len_frame+len_pream,np.float32)
+        s_out=self.create_socket_out(t_rem_pream, "s_out",self.len_frame,np.float32)
+        self.create_codelet(t_rem_pream, lambda slf, lsk, fid: self.remove_preamble(lsk[s_in], lsk[s_out]))
 
 
-N_source = 2048
-
-M = np.ndarray(shape = (1,1),  dtype = np.int32)
-M[0] = 2
-
-Ns = N_source//M[0]
-
-alpha = np.ndarray(shape = (1,1),  dtype = np.float32)
-alpha[0] = 0.2
-
-in_fft= np.ndarray(shape = (1,Ns[0]),  dtype = np.float32)
-in_fft[0, :] = np.zeros(Ns[0])
-
-sigma = np.ndarray(shape = (1,1),  dtype = np.float32)
-sigma[0] = 0.0001
-
-Ts = np.ndarray(shape = (1,1),  dtype = np.float32)
-Ts[0] = 0.01
-
-N_pream=64
-for i in range(10):
-
-    src = py_aff3ct.module.source.Source_random(N_source)
-    cstl = py_aff3ct.tools.constellation.Constellation_QAM(M)
-    mdm  = py_aff3ct.module.modem.Modem_generic(N_source, cstl)
-    mdm("modulate").info()
-    pream=preamble(N_pream, N_source)
-    pream("insert_preamble").info()
-    chn  = py_aff3ct.module.channel.Channel_AWGN_LLR((N_source+N_pream))
-    chn("add_noise").info()
-    dec  = py_aff3ct.module.decoder.Decoder_repetition_std(N_source,N_source)
-    # syn = Frequency_Synchronizer(2*Ns)
-
-
-    mdm["modulate::X_N1"].bind(src["generate::U_K"])
-    pream["insert_preamble::s_in"].bind(mdm["modulate::X_N2"])
-    chn[ "add_noise::X_N"].bind(pream[ "insert_preamble::s_out"])
-    pream["remove_preamble::s_in"].bind(chn[ "add_noise::Y_N"])
-    mdm["demodulate::Y_N1"].bind(pream["remove_preamble::s_out"])
-    dec[ 'decode_siho::Y_N' ].bind(mdm[ 'demodulate::Y_N2'])
-
-    chn[ 'add_noise::CP'].bind(sigma)
-    mdm['demodulate::CP'].bind(sigma)
-    # syn['synchronize::IN_m'].bind(M)
-    # syn['synchronize::IN_ts'].bind(Ts)
-    # syn['synchronize::IN_fft'].bind(in_fft)
-    # syn['synchronize::IN_alpha'].bind(alpha)
-    # syn['desynchronize::IN_ts1'].bind(Ts)
-
-    src("generate").exec()
-    mdm("modulate").exec()
-    pream("insert_preamble").exec()
-    # syn('desynchronize').exec()
-    chn("add_noise").exec()
-    # syn('synchronize').exec()
-    pream("remove_preamble").exec()
-    mdm('demodulate').exec()
-    dec('decode_siho').exec()
-
-    # in_fft[0, :] = syn['synchronize::OUT_fft'][:]
