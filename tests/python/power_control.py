9fdb96ae1ebeca85762e7c9d6a41c71546d3d532
Oussama Bastamy <obastamy@enseirb-matmeca.fr>
2021-05-18 18:50:26 +0200

    edit

--- tests/python/power_control.py
+++ tests/python/power_control.py
@@ -5,11 +5,6 @@ sys.path.insert(0, '../../build/lib')
 from py_aff3ct.module.py_module import Py_Module
 
 
-switch = 18 #physical pin num attached to the switch
-
-
-
-
 class power_control(Py_Module):
 
     def on(self): #off
@@ -36,14 +31,20 @@ class power_control(Py_Module):
         Py_Module.__init__(self)
         self.name = "power_control"
         
+        switch = 18 #physical pin num attached to the switch
         GPIO.setmode(GPIO.BOARD)   
         GPIO.setup(switch, GPIO.OUT) 
         self.state=0
 
         swtch = self.create_task('change_state')
         sin_change_state = self.create_socket_in(swtch, "X_N", 1, np.int32)  # send 1 to change state 
-        sout_state = self.create_socket_out(swtch, "Y_N", 1, np.float32)   #current state of switch
+        sout_state = self.create_socket_out(swtch, "Y_N", 1, np.int32)   #current state of switch
 
         # properties
         self.create_codelet(swtch, lambda slf, lsk,
                             fid: self.change_state(lsk[sin_change_state], lsk[sout_state]))
+
+
+
+
+
